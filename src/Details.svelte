e92ecce38ce114f9be216e9e7939f3ec14131b0b
Christopher LaChance <christopher.t.lachance@gmail.com>
2020-12-04 18:36:59 -0600

    Fixed a typo.

--- src/Details.svelte
+++ src/Details.svelte
@@ -89,7 +89,7 @@
         <img id="details-cover" src={products[index].cover} alt={`${products[index].name} Cover`}>
         <div id="details-text">
             <h2>{products[index].name.replaceAll("_"," ")}</h2>
-            <p>Algebra Readiness Builders are designed to enhance the math skills of your students by introducing and honing a concept through repeition and at home practice. Each book contains 80 builders with which you can build a foundation for the next level.</p>
+            <p>Algebra Readiness Builders are designed to enhance the math skills of your students by introducing and honing a concept through repetition and at home practice. Each book contains 80 builders with which you can build a foundation for the next level.</p>
             <p>Here are some of the skills this builder helps develop:</p>
             <div id="details-skills">
                 {#each products[index].skill_list as skill}
